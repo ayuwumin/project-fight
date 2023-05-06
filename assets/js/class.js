237600ec0f721273cbe10b3a3d9d04441d1a637b
Aether <tifsnunes@gmail.com>
2023-05-06 17:20:50 -0300

    creating attacks

--- assets/js/class.js
+++ assets/js/class.js
@@ -82,18 +82,36 @@ class Stage {
 
     update(){
        // fighter1
-       this.fighterEl.querySelector('.name').innerHTML = `${this.fighter1.name} - ${this.fighter1.life} HP`
+       this.fighterEl.querySelector('.name').innerHTML = `${this.fighter1.name} - ${this.fighter1.life.toFixed(1)} HP`
        let f1Pct = (this.fighter1.life / this.fighter1.maxLife) * 100
        this.fighter1El.querySelector('.bar').style.width = `${f1Pct}%`
        
        // fighter2
-       this.fighter2El.querySelector('.name').innerHTML = `${this.fighter2.name} - ${this.fighter2.life} HP`
+       this.fighter2El.querySelector('.name').innerHTML = `${this.fighter2.name} - ${this.fighter2.life.toFixed(1)} HP`
        let f2Pct = (this.fighter2.life / this.fighter2.maxLife) * 100
        this.fighter2El.querySelector('.bar').style.width = `${f2Pct}%`
     }
 
     doAttack(attacking, attacked){
-        console.log(`${attacking.name} is attacking ${attacked.name}`)
+        if(attacking.life <= 0 || attacked.life <= 0){
+            console.log(`Atacando player morto.`)
+            return
+        }
+
+        let attackFactor = (Math.random() * 2).toFixed(2)
+        let defenseFactor = (Math.random() * 2).toFixed(2)
+
+        let actualAttack = attacking.attack * attackFactor
+        let actualDefense = attacked.defense * defenseFactor
+
+        if(actualAttack > actualDefense){
+            attacked.life -= actualAttack
+            console.log(`${attacking.name} causou ${actualAttack.toFixed(2)} de dano em ${attacked.name}`)
+        } else {
+            console.log(`${attacked.name} conseguiu defender`)
+        }
+
+        //console.log(`${attacking.name} is attacking ${attacked.name}`)
         this.update()
     }
 }
