5a93e98c63fcdfe8b5d0423dac767176a08b3b16
Aether <tifsnunes@gmail.com>
2023-05-08 15:24:10 -0300

    log

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+let log = new log(document.querySelector('.log'))
 let char = new Knight('Kaveh')
 let monster = new littleMonster()
 
@@ -6,5 +7,8 @@ const stage = new Stage(
     char,
     monster,
     document.querySelector('#char'),
-    document.querySelector('#monster')
+    document.querySelector('#monster'),
+    log
 )
+
+stage.start()
