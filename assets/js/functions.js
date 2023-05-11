920e387f5c35ebe9ae0825c16a5754b4e6be880c
Aether <tifsnunes@gmail.com>
2023-05-11 14:53:33 -0300

    creating log

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -80,7 +80,41 @@ const stage = {
         this.fighter2El.querySelector('.bar').style.width = `${f2Pct}%`
     },
     doAttack(attacking, attacked){
+        if(attacking.life <= 0 || attacked.life <= 0){
+            log.addMessage('hitting dead player')
+            return
+        }
 
+        const attackFactor = (Math.random () * 2).toFixed(2)
+        const defenseFactor = (Math.random () * 2).toFixed(2)
+
+        const actualAttack = attacking.attack * attackFactor
+        const actualDefense = attacked.defense * defenseFactor
+
+        if(actualAttack > actualDefense){
+            attacked.life -= actualAttack
+            attacked.life = attacked.life <0 ? 0 : attacked.life
+            log.addMessage(`${attacking.name} caused ${actualAttack.toFixed(2)} damage to ${attacked.name}`)
+        }else {
+            log.addMessage(`${attacked.name} managed to defend...`)
+        }
+
+        this.update()
     }
 }
 
+const log = {
+    list: [],
+    addMessage(msg) {
+        this.list.push(msg)
+        this.render()
+    },
+    render(){
+        const logEl = document.querySelector('.log')
+        logEl.innerHTML = ''
+
+        for(let i in this.list){
+            logEl.innerHTML += `<li>${this.list[i]}</li>`
+        }
+    }
+}
